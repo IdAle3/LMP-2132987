526d351655d47bfbb72382a3559571a66d8a86ea
IdAle3 <idalia.delgadom@uanl.edu.mx>
2024-09-17 21:13:01 -0600

    step 1 django

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('polls.urls')),  # Esto enlaza las rutas de 'polls' a la raíz del sitio
+    path('polls/', include('polls.urls')),  # Esto enlaza las rutas de la app polls
     path('admin/', admin.site.urls),
 ]
 
